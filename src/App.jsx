bd1e36f6f8bea1f6472c17b54985d28205d591d4
prakashsingh62 <prakashkumarsing@gmail.com>
2025-12-19 19:49:33 +0530

    mount RFQPage directly to trigger data fetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,5 @@
-import { Routes, Route } from "react-router-dom";
 import RFQPage from "./pages/RFQPage";
-import AuditPage from "./pages/AuditPage";
 
 export default function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<RFQPage />} />
-      <Route path="/audit" element={<AuditPage />} />
-    </Routes>
-  );
+  return <RFQPage />;
 }
