417a6f229bc0beeba4655a889ad76b2b9d0d9bf1
prakashsingh62 <prakashkumarsing@gmail.com>
2025-12-19 17:22:58 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import AuditPage from "./pages/AuditPage";
 import RFQDashboard from "./pages/RFQDashboard";
+import RFQPage from "./pages/RFQPage";
 
 export default function App() {
   return (
     <Routes>
-      <Route path="/" element={<RFQDashboard />} />
+      <Route path="/" element={<RFQPage />} />
+      <Route path="/dashboard" element={<RFQDashboard />} />
       <Route path="/audit" element={<AuditPage />} />
     </Routes>
   );
 }
-// trigger vercel rebuild
