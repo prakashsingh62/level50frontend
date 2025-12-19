369849182ab0751cdc4feef8b52eb5733cd65643
prakashsingh62 <prakashkumarsing@gmail.com>
2025-12-19 18:11:38 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,5 @@
-import { Routes, Route } from "react-router-dom";
-import AuditPage from "./pages/AuditPage";
-import RFQDashboard from "./pages/RFQDashboard";
 import RFQPage from "./pages/RFQPage";
 
 export default function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<RFQPage />} />
-      <Route path="/dashboard" element={<RFQDashboard />} />
-      <Route path="/audit" element={<AuditPage />} />
-    </Routes>
-  );
+  return <RFQPage />;
 }
